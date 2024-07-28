5fbf8aa3c778cca8c8823e6a768cd04ef019fc37
sansidsac <kssanju04@gmail.com>
2024-07-28 23:26:51 +0530

    Added Backend folder

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://localhost:27017/";
+
+async function connectToMongo() {
+    await mongoose.connect(mongoURI);
+    console.log("Connected to Mongo Successfully");
+  }
+  
+  module.exports = connectToMongo;
