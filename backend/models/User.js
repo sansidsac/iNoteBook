6daffdbbae94757809367a24142ce0fa5400c9a8
sansidsac <kssanju04@gmail.com>
2024-07-30 00:21:06 +0530

    Added validity checker and collection for Authentication requests in Thunder Client

--- backend/models/User.js
+++ backend/models/User.js
@@ -21,5 +21,5 @@ const UserSchema = new Schema({
         default: Date.now
     }
 });
-
-module.exports = mongoose.model('user', UserSchema);
+const User = mongoose.model('user', UserSchema);
+module.exports = User;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,42 @@
 const express = require('express');
 const User=require('../models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 
-// Create a User using: POST "/api/auth/". Doesn't require Auth
-router.post('/', (req, res) => {
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
+// Create a User using: POST "/api/auth/createuser". No login required
+router.post('/createuser', [
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must be atleast 5 characters').isLength({ min: 5 })
+], async(req, res) => {
+// If there are errors, return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    // Check whether the user with this email exists already
+    try{
+    
+    let user = await User.findOne({ email: req.body.email });
+    if(user){
+        return res.status(400).json({error:"Sorry a user with this email already exists"})
+    }
+    user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    })
+        // .then(user=>res.json(user))
+        // .catch(err=>{console.log(err);
+        // res.json({error:"Please enter a unique value for email"})});
+
+    res.json({user})
+
+    }catch(error){
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
 })
 
 module.exports = router
