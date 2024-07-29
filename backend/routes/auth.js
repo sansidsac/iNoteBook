f568c330f2e2adf698dc7f543d8f5c8e5e0b94cf
sansidsac <kssanju04@gmail.com>
2024-07-30 05:17:19 +0530

    Created a Route for User Login using Credentials

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
 
 const router = express.Router();
+const JWT_SECRET="Sanjuisagoodb$oy";
 
 
 // Create a User using: POST "/api/auth/createuser". No login required
@@ -42,7 +43,6 @@ router.post('/createuser', [
             id:user.id
         }
     }
-    const JWT_SECRET="Sanjuisagoodb$oy";
     const authtoken=jwt.sign(data,JWT_SECRET);
     res.json({authtoken});
     
@@ -53,5 +53,45 @@ router.post('/createuser', [
         res.status(500).send("Internal Server Error");
     }
 })
+
+
+// Authenticate a User using: POST "/api/auth/login". No login required
+router.post('/login', [
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password cannot be blank').exists(),
+] , async(req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const {email,password}=req.body;
+    try{
+        let user=await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error:"Please try to login with correct credentials"});
+        }
+
+        const passwordCompare=await bcrypt.compare(password,user.password);
+        if(!passwordCompare){
+            return res.status(400).json({error:"Please try to login with correct credentials"});
+        }
+
+        const payload={
+            user:{
+                id:user.id
+            }
+        }
+
+        const authtoken=jwt.sign(payload,JWT_SECRET);
+
+        res.json({authtoken});
+
+    }catch(error){
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+})
+
 
 module.exports = router
