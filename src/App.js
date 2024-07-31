f623b26cd179cceb67af46af9095c18e20c0b3c4
sansidsac <kssanju04@gmail.com>
2024-08-01 03:45:02 +0530

    Added NavBar and Home, used useLocation for accessing current active link

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import './App.css';
-import{
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import NoteState from "./context/notes/noteState";
+
 function App() {
   return (
     <>
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/about" element={<About />} />
-        </Routes>
-      </Router>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <div className="container my-3">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/about" element={<About />} />
+            </Routes>
+          </div>
+        </Router>
+      </NoteState>
     </>
   );
 }
