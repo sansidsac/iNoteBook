60a5b1335b5e619fb9bb0de425600a8d4e14b0f4
sansidsac <kssanju04@gmail.com>
2024-08-07 00:51:15 +0530

    Added about

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const About = () => {
-  return <div>This is About </div>;
+  return <div>This is my first complete MERN stack project </div>;
 };
 
 export default About;
